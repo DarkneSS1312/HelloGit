66249f891bacca3ef57c24f19e913963102cfbee
Mohammed <300137495@monboreal.ca>
2024-10-19 15:03:13 -0800

    Ajout de class Revue

---
+++ Revue.cs
@@ -0,0 +1,24 @@
+﻿using static Program;
+
+namespace AppConsoleLivres
+{
+    public class Revue : Livre
+    {
+        public int Numero { get; set; }
+        public int Annee { get; set; }
+
+        public Revue(string titre, string auteur, int nbrPages, int numero, int annee)
+            : base(titre, auteur, nbrPages)
+        {
+            Numero = numero;
+            Annee = annee;
+        }
+
+        public override void AfficherDetails()
+        {
+            base.AfficherDetails();
+            Console.WriteLine($"Numéro: {Numero}, Année: {Annee}");
+        }
+    }
+}
+
